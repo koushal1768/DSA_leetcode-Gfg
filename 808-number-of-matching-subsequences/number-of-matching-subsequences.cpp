6fbba1bce6b7c1d403b59bcf65f0ba8cbfba2a63
koushal1768 <151174225+koushal1768@users.noreply.github.com>
2025-08-20 19:00:51 +0530

    Time: 73 ms (78.23%) | Memory: 55.5 MB (67.05%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.cpp
@@ -15,9 +15,7 @@ public:
            for(char c:word)
             { int ch=c-'a';
               const auto& vec=chvec[ch];
-auto it = std::upper_bound(vec.begin(), vec.end(), index);
-
-            
+               auto it = std::upper_bound(vec.begin(), vec.end(), index);
                 if(it==vec.end())
                 {
                    ok=0;
@@ -32,3 +30,9 @@ auto it = std::upper_bound(vec.begin(), vec.end(), index);
     }
 };
  
+//approach
+//1) make an adjlist if int type of size 26 which store the index of char of string s just like char a will comes 2 times at index 2 and at index 6 then in adj[a]->2,6
+//2) now iterate on each word in the vector words.
+//3)take a 2 variable ok and index where index represent the index of the selected char of string s 
+//3) now iterate on each char ch of selected word
+//4)hum adj list m chechk karenge corresponding to that word that does any position is present which is greater than index if possible then we update the index check for the next word if not present then that word is not the part of that word so we direcly look for the another word because in that it is not possible
